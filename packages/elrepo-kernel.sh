20b433a287309a183696a617f25f4c0c5bd79ace
Esa Varemo <git@esav.fi>
2021-08-31 11:15:40 +0300

    Add separate script to download nmstate and elrepo kernel as necessary

---
+++ packages/elrepo-kernel.sh
@@ -0,0 +1,2 @@
+wget -A "*.rpm" -m -nH --cut-dirs 10 -l 1 --execute robots=off \
+    https://elrepo.org/linux/kernel/el8/x86_64/RPMS/
---
+++ packages/nmstate.sh
@@ -0,0 +1,2 @@
+wget -A "*.rpm" -m -nH --cut-dirs 10 \
+    https://copr-be.cloud.fedoraproject.org/results/varesa/nmstate-git/centos-stream-8-x86_64/02675965-nmstate/
